fb87a16ecef9e794f678caec329aa0ec1be4ea16
gonzalez_sebastian <gonzalezsebastian1209@gmail.com>
2023-09-14 00:44:19 -0300

    con bootstrap 5

---
+++ src/componentes/NavBar/NavBar.js
@@ -0,0 +1,52 @@
+/* import React from 'react'
+import './NavBaer.css'
+
+const NavBar = () => {
+    return (
+       <nav>
+        <h3>GamerShop</h3>
+        <ul>
+            <li>Monitores</li>
+            <li>Mouse</li>
+            <li>PC</li>
+            <li>Teclado</li>
+        </ul>
+       </nav>
+    )
+}
+export default NavBar */
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function NavBar() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">GamerShop</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Inicio</Nav.Link>
+            <Nav.Link href="#link">Perifericos</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavBar; 
